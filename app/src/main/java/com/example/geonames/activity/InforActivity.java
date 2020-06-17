f1e79d7b381f209b189759c785dd9ccff32fe4bf
DinhSang <dinhsangdroid@gmail.com>
2020-06-17 12:49:30 +0700

    GEONAMES-$: Fix - Internet Connect

--- app/src/main/java/com/example/geonames/activity/InforActivity.java
+++ app/src/main/java/com/example/geonames/activity/InforActivity.java
@@ -1,14 +1,21 @@
 package com.example.geonames.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.geonames.R;
 import com.example.geonames.model.Geoname;
 
@@ -28,10 +35,25 @@ public class InforActivity extends AppCompatActivity {
 
     private void initData() {
         getData();
-        Glide.with(this).load("https://img.geonames.org/flags/x/" + geoname.getCountryCode().toLowerCase() + ".gif").into(imageViewCo);
+
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(this);
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(30f);
+        circularProgressDrawable.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        circularProgressDrawable.start();
+
+        Glide.with(this).load("https://img.geonames.org/flags/x/" + geoname.getCountryCode().toLowerCase() + ".gif")
+                .placeholder(circularProgressDrawable)
+                .into(imageViewCo);
         textViewCountryName.setText(geoname.getCountryName());
         textViewName.setText(geoname.getCountryName());
-        textViewPopulation.setText(geoname.getPopulation());
+
+        if (geoname.getPopulation() == null || geoname.getPopulation().equals("0") || geoname.getPopulation().equals("")) {
+            textViewPopulation.setText("No data found");
+        } else {
+            textViewPopulation.setText(geoname.getPopulation());
+        }
+
         textViewAreaInSqKm.setText(geoname.getAreaInSqKm() + " SqKm");
     }
 
--- app/src/main/java/com/example/geonames/activity/MainActivity.java
+++ app/src/main/java/com/example/geonames/activity/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.geonames.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 
 import com.example.geonames.R;
 import com.example.geonames.adapter.CountryAdapter;
@@ -21,6 +31,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CountryAdapter countryAdapter;
+    private CardView cardViewReload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +39,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initEvent();
-        initData();
-
-
+        internet();
     }
 
     private void initData() {
+        final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "Loading data",
+                "Loading. Please wait...", true);
+        dialog.setCancelable(false);
+        dialog.show();
         new JsonTaskFull().jsonReader("http://api.geonames.org/countryInfoJSON?formatted=true&lang=it&username=sangdeptrai&style=full",
                 new JsonTaskFull.JsonTaskFullListener() {
                     @Override
                     public void onLoadSuccess(ArrayList<Geoname> geonameArrayList) {
+                        cardViewReload.setVisibility(View.GONE);
+                        dialog.cancel();
                         countryAdapter = new CountryAdapter(MainActivity.this, geonameArrayList);
                         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
                         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -47,17 +62,24 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onLoadFail(String error) {
-
+                        dialog.cancel();
+                        internet();
                     }
                 });
     }
 
     private void initEvent() {
-
+        cardViewReload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                internet();
+            }
+        });
     }
 
     private void initView() {
         recyclerView = findViewById(R.id.rv_list_country);
+        cardViewReload = findViewById(R.id.cv_reload);
     }
 
     @Override
@@ -82,5 +104,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return true;
+    }
+
+    private void internet() {
+        if (isOnline()) {
+            initData();
+        } else {
+            android.app.AlertDialog.Builder builder1 = new android.app.AlertDialog.Builder(this);
+            builder1.setTitle("Info");
+            builder1.setMessage("Internet not available, Cross check your internet connectivity and try again.");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton(
+                    "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                            cardViewReload.setVisibility(View.VISIBLE);
+                        }
+                    });
+            android.app.AlertDialog alert11 = builder1.create();
+            alert11.setCancelable(false);
+            alert11.show();
+        }
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager conMgr = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
+
+        if(netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()){
+            Toast.makeText(this, "No Internet connection!", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            Toast.makeText(this, "Internet connection!", Toast.LENGTH_LONG).show();
+            return true;
+        }
     }
 }
